e35a90d64f7ae940254f34d730b429e6338842a8
Sofia Genchi <sofigen16@gmail.com>
2022-09-06 12:47:48 -0300

    home

---
+++ portfolio/src/components/Home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/sofia-genchi" className="home__social-icon" target={'_blank'}><i class="uil uil-linkedin"></i></a>
+
+        <a href="https://github.com/SofiaGenchi" className="home__social-icon" target={'_blank'}><i class="uil uil-github"></i></a>
+    </div>
+  )
+}
+
+export default Social
